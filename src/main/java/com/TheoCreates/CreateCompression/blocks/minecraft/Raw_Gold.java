96ecacdf000191ca02286d43ab0d8495a4dd331d
Theo Morris <pandazflipperpanda706@gmail.com>
2024-04-14 17:50:53 +1200

    1.20.1 fixes

--- src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Raw_Gold.java
+++ src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Raw_Gold.java
@@ -2,11 +2,10 @@ package com.TheoCreates.CreateCompression.blocks.minecraft;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
 
 public class Raw_Gold extends Block {
     public Raw_Gold() {
-        super(Properties.of(Material.STONE)
+        super(Properties.of()
             .sound(SoundType.STONE)
             .strength(5.0f, 6.0f));
     }
