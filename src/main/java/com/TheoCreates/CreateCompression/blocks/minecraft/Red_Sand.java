24b9d207346328636a74af0445554132b5b0df9a
TheoCreates <pandazflipperpanda706@gmail.com>
2025-10-20 00:16:47 +1300

    fixed loot tables

--- src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Red_Sand.java
+++ src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Red_Sand.java
@@ -1,14 +1,24 @@
 package com.TheoCreates.CreateCompression.blocks.minecraft;
 
 
-import net.minecraft.world.level.block.Block;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
+public class Red_Sand extends FallingBlock {
+    public static final MapCodec<Red_Sand> CODEC = simpleCodec(Red_Sand::new);
 
-public class Red_Sand extends Block {
-    public Red_Sand() {
-        super(Properties.of()
-            .sound(SoundType.SAND)
-            .strength(0.5f));
+    public Red_Sand(BlockBehaviour.Properties props) {
+        super(props);
+    }
+
+    public static Red_Sand create() {
+        return new Red_Sand(BlockBehaviour.Properties.of().sound(SoundType.SAND).strength(0.5F));
+    }
+
+    @Override
+    protected MapCodec<? extends FallingBlock> codec() {
+        return CODEC;
     }
 }
--- src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Redstone.java
+++ src/main/java/com/TheoCreates/CreateCompression/blocks/minecraft/Redstone.java
@@ -1,15 +1,34 @@
 package com.TheoCreates.CreateCompression.blocks.minecraft;
 
-
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
 
 public class Redstone extends Block {
     public Redstone() {
         super(Properties.of()
+            .mapColor(MapColor.COLOR_RED)
             .sound(SoundType.METAL)
             .requiresCorrectToolForDrops()
-            .strength(5.0f, 6.0f));
+            .strength(5.0F, 6.0F));
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction side) {
+        return 15;
+    }
+
+    @Override
+    public int getDirectSignal(BlockState state, BlockGetter level, BlockPos pos, Direction side) {
+        return 15;
     }
 }
